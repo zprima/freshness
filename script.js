9c1a390b8dab77c58ac538a6976566e408d81f1c
Primoz <sg.prima@gmail.com>
2019-04-30 20:45:32 +0200

    fresh

---
+++ script.js
@@ -0,0 +1,24 @@
+const FRESHNESS_UI = 'freshness_ui';
+
+console.log("script here");
+
+// add freeshness_ui class to body
+var bodyEle = document.body;
+bodyEle.classList.add(FRESHNESS_UI);
+
+// toggle freshness ui class on the body element
+function toggleFreshness() {
+  var status = bodyEle.classList.toggle(FRESHNESS_UI);
+  console.log("toggle status", status);
+  return status;
+}
+
+// keyboard shortcut for toggle
+function handleToggle(event) {
+  // cmd + j
+  if (event.metaKey && event.which == 74) {
+    toggleFreshness();
+    event.preventDefault();
+  }
+}
+window.addEventListener('keydown', handleToggle, false);
